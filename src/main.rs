7286c1bf89246d444d30eef11f0336992932dd2f
James Cuzella <james.cuzella@lyraphase.com>
2023-09-07 15:22:48 -0600

    Add simple control flow examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    // let number = 3; // condition was true
+    let number = 7; // condition was false
+
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Does NOT compile!
+    // expected `bool`, found integer
+    // if number {
+    //     println!("number was three");
+    // }
+
+    // Explicit boolean expressions must be used
+    if number != 0 {
+        println!("number was something other than zero");
+    }
 }
